f8b9fc6b95d7563923773a09e5d976eea382c7fa
Arvin <arvin4943@gmail.com>
2025-05-07 10:00:35 +0330

    Add Remove Duplicates from Sorted Array with a more efficient run time

--- 26 Remove Duplicates from Sorted Array.py
+++ 26 Remove Duplicates from Sorted Array.py
@@ -10,4 +10,19 @@ class Solution:
                 nums[iw] = prev
                 iw += 1
         return iw
-        
+
+#%% New approach
+# Although this code is not efficient in aspect of space, it works efficient in time.
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        j = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[j]:
+                j += 1
+                nums[j] = nums[i]
+        return j + 1
