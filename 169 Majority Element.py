8305f1302e1f84b0ab1d189bc1c532b3a8cee785
Arvin <arvin4943@gmail.com>
2025-05-10 16:10:12 +0330

    169 Majority Element
    Add a candidate/count algorithm as like as Ali's code

--- 169 Majority Element.py
+++ 169 Majority Element.py
@@ -17,3 +17,25 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
         return nums[len(nums) // 2]
+    
+#%% Arvin's approach:
+# This approach is as like as Ali's approach and this algorithm is famous to Boyer-Moore voting algorithm.
+# I and Ali talked about this algorithm a lot and had a comprehensive analysis in a cafe.
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count += 1
+            elif count != 0 and num == candidate:
+                    count += 1                
+            elif count != 0 and num != candidate:
+                count -= 1
+        return candidate
