22799d94e406842b0cd28ee75fbe75239e2214ca
Arvin <arvin4943@gmail.com>
2025-05-10 17:14:03 +0330

    Add Majority Element with Candidate/Count algorithm

--- 169 Majority Element.py
+++ 169 Majority Element.py
@@ -17,3 +17,25 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
         return nums[len(nums) // 2]
+
+
+#%% Arvin's code
+# We have talked about this algorithm in a cafe. This algorithm is called Boyer-Moore or Candidate/Count
+# algorith.
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count += 1
+            elif count != 0 and num == candidate:
+                    count += 1                
+            elif count != 0 and num != candidate:
+                count -= 1
+        return candidate
