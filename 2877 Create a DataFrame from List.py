45d908746da2c55652019e58f040f99132eeda53
Arvin <arvin4943@gmail.com>
2025-05-20 11:37:17 +0330

    Add some pandas problems

--- 2877 Create a DataFrame from List.py
+++ 2877 Create a DataFrame from List.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+def createDataframe(student_data: list[list[int]]) -> pd.DataFrame:
     dataframe = pd.DataFrame(student_data, columns=['student_id', 'age'])
     return dataframe
